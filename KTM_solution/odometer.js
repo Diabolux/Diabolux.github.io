1834dd7ca7b960b403d9fdf5ae5b752c4c225d3b
Diabolux <58630829+Diabolux@users.noreply.github.com>
2020-12-10 19:04:24 +0100

    ajout js

---
+++ KTM_solution/odometer.js
@@ -0,0 +1,9 @@
+window.odometerOptions = {
+    format: '( ddd),dd'
+};
+
+setTimeout(function(){
+    nbBoutique.innerHTML = 100;
+    nbChiffreAffaire.innerHTML = 80000;
+    nbCommande.innerHTML = 2500;
+},1000);
