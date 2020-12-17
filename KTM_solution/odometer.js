a4ef2d9d40b1d2ec45d67e997ef239ccadda9b04
Diabolux <58630829+Diabolux@users.noreply.github.com>
2020-12-17 20:01:17 +0100

    modification js

--- KTM_solution/odometer.js
+++ KTM_solution/odometer.js
@@ -2,8 +2,22 @@ window.odometerOptions = {
     format: '( ddd),dd'
 };
 
-setTimeout(function(){
-    nbBoutique.innerHTML = 100;
-    nbChiffreAffaire.innerHTML = 80000;
-    nbCommande.innerHTML = 2500;
-},1000);
+function odometer(entries, observer) {
+    let test = entries[0]
+    if (test.isIntersecting) {
+        console.log(entries);
+        nbBoutique.innerHTML = 100;
+        nbChiffreAffaire.innerHTML = 80000;
+        nbCommande.innerHTML = 2500;
+    }
+
+}
+
+var options = {
+    threshold: 0.7
+}
+
+let observer = new IntersectionObserver(odometer, options);
+
+let chiffresCles = document.querySelector('.chiffresCles');
+observer.observe(chiffresCles);
